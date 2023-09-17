a2af76e707e0cc0ee310094f6c7bee05712a4128
Michael <96359292+biggerpipicek@users.noreply.github.com>
2023-09-17 19:01:11 +0200

    edited the script file

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const paragraph = document.getElementsByClassName("card-text");
+document.addEventListener("DOMContentLoaded", function() {
+    const paragraph = document.getElementById("card-text");
 
-// Get the number of characters you want to display before fading
-const characterLimit = 55; // Change this to your desired character limit
+    // Get the number of characters you want to display before fading
+    const characterLimit = 50; // Change this to your desired character limit
 
-// Check if the text exceeds the character limit
-if (paragraph.textContent.length > characterLimit) {
-    // Trim the text to the character limit and add "..."
-    const truncatedText = paragraph.textContent.slice(0, characterLimit) + "...";
-    
-    // Update the paragraph text
-    paragraph.textContent = truncatedText;
-}
+    // Check if the text exceeds the character limit
+    if (paragraph.textContent.length > characterLimit) {
+        // Trim the text to the character limit and add "..."
+        const truncatedText = paragraph.textContent.slice(0, characterLimit) + "...";
+        
+        // Update the paragraph text
+        paragraph.textContent = truncatedText;
+    }
+});
