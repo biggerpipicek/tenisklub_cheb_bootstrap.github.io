6de261b9f1aebdd9ebb17a4fe982e6d492f64c95
Michael <96359292+biggerpipicek@users.noreply.github.com>
2023-09-17 19:10:56 +0200

    edited the text truncate in the script file

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const paragraph = document.getElementById("card-text");
+function truncate(string, txtLength) {
+    return (string.length > txtLength) ?
+        string.slice(0, txtLength - 1) + "..." : string;
+}
 
-    // Get the number of characters you want to display before fading
-    const characterLimit = 50; // Change this to your desired character limit
-
-    // Check if the text exceeds the character limit
-    if (paragraph.textContent.length > characterLimit) {
-        // Trim the text to the character limit and add "..."
-        const truncatedText = paragraph.textContent.slice(0, characterLimit) + "...";
-        
-        // Update the paragraph text
-        paragraph.textContent = truncatedText;
-    }
-});
+truncate(document.getElementsByClassName("card-text"), 55);
